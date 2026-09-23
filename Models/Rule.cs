93fda234fc4255012766f2dd18047e92b181cbd4
Mathieu-c360 <Mathieu.Colle@Condorcet.be>
2026-09-23 23:25:21 +0200

    feat : finition de la méthode de règle

--- Models/Rule.cs
+++ Models/Rule.cs
@@ -3,22 +3,27 @@
 public class Rule
 {
     public static int NumberMax=21;
-    public static void RuleTwentyOne(List<Player> players)
+    public static List<Player> RuleTwentyOne(List<Player> players)
     {
-        var listePlayerMaybeWinners=new List<Player>();;
+        var listPlayerMaybeWinners=new List<Player>();
+        var listPotentialWinners=new List<Player>();
         int addition=0;
         foreach(Player player in players)
         {
             if(VerifValueNumbers(addition))
-                listePlayerMaybeWinners.Add(player);
+                listPlayerMaybeWinners.Add(player);
         }
-        var allPlayersOrder=listePlayerMaybeWinners.OrderByDescending(p=>p.ScoreCards)
+        var allPlayersOrder=listPlayerMaybeWinners.OrderByDescending(p=>p.ScoreCards)
                                                     .ToList();
-        var maxValue=allPlayersOrder.First()                                 
+        var maxValue=allPlayersOrder.First().ScoreCards;   
+        listPotentialWinners.Add(allPlayersOrder.First()); 
+        allPlayersOrder.RemoveAt(0);
         foreach(Player player in allPlayersOrder)
         {
-            
+            if(player.ScoreCards==maxValue)
+                listPotentialWinners.Add(player);
         }
+        return listPotentialWinners;
     }
     public static bool VerifValueNumbers(int addition)
     {
--- obj/Debug/net8.0/BlackJack.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlackJack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c29871b728106f44de6940a7a58e84544b5e51ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99678b53e5d29acb5313db72de3a710533045a41")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
